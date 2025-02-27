265f54f1e47d88bcbb76fc6f7c42784b8ea7f45a
Brian W Bush <code@functionally.io>
2025-02-26 17:12:36 -0700

    Formatted

--- dispatch/dispatch.go
+++ dispatch/dispatch.go
@@ -1,77 +1,77 @@
 package dispatch
 
 import (
-    "bytes"
-    "fmt"
-    "strings"
+	"bytes"
+	"fmt"
+	"strings"
 
-    "github.com/google/shlex"
-    "github.com/spf13/cobra"
+	"github.com/google/shlex"
+	"github.com/spf13/cobra"
 
 	"github.com/functionally/cyfryngwr/rss"
 )
 
 var (
-    version   = "dev"
-    gitCommit = "none"
+	version   = "dev"
+	gitCommit = "none"
 )
 
 func Run(input string) (string, error) {
 
-    var result bytes.Buffer
-    var errResult error = nil
+	var result bytes.Buffer
+	var errResult error = nil
 
-    var rootCmd = &cobra.Command{
-        Use:   "/",
-        Short: "Cyfryngwr agent",
-        Run: func(cmd *cobra.Command, args []string) {
-	    cmd.Help()
-        },
-    }
-    rootCmd.SetOut(&result)
+	var rootCmd = &cobra.Command{
+		Use:   "/",
+		Short: "Cyfryngwr agent",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	rootCmd.SetOut(&result)
 
-    var versionCmd = &cobra.Command{
-        Use:   "version",
-        Short: "Reply with version information",
-        Run: func(cmd *cobra.Command, args []string) {
-	  result.WriteString(fmt.Sprintf("Cyfryngwr %s (%s)", version, gitCommit))
-        },
-    }
-    rootCmd.AddCommand(versionCmd)
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Reply with version information",
+		Run: func(cmd *cobra.Command, args []string) {
+			result.WriteString(fmt.Sprintf("Cyfryngwr %s (%s)", version, gitCommit))
+		},
+	}
+	rootCmd.AddCommand(versionCmd)
 
-    var rssCmd = &cobra.Command{
-      Use: "rss",
-      Short: "Access RSS feeds",
-      Run: func(cmd *cobra.Command, args []string) {
-	    cmd.Help()
-      },
-    }
-    {
-      var url string
-      var rssFetchCmd = &cobra.Command{
-        Use: "fetch",
-        Short: "Fetch items from RSS feed",
-        Run: func(cmd *cobra.Command, args []string) {
-    	feed, err := rss.FetchRSS(url)
+	var rssCmd = &cobra.Command{
+		Use:   "rss",
+		Short: "Access RSS feeds",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	{
+		var url string
+		var rssFetchCmd = &cobra.Command{
+			Use:   "fetch",
+			Short: "Fetch items from RSS feed",
+			Run: func(cmd *cobra.Command, args []string) {
+				feed, err := rss.FetchRSS(url)
+				if err != nil {
+					errResult = err
+				}
+				result.WriteString(fmt.Sprintf("%s\n%s", feed.Title, feed.Items[0].Title))
+			},
+		}
+		rssFetchCmd.Flags().StringVarP(&url, "url", "u", "", "URL for feed")
+		rssCmd.AddCommand(rssFetchCmd)
+	}
+	rootCmd.AddCommand(rssCmd)
+
+	args, err := shlex.Split(strings.TrimPrefix(input, "/"))
 	if err != nil {
-	  errResult = err
-	} 
-	result.WriteString(fmt.Sprintf("%s\n%s", feed.Title, feed.Items[0].Title))
-        },
-      }
-      rssFetchCmd.Flags().StringVarP(&url, "url", "u", "", "URL for feed")
-    rssCmd.AddCommand(rssFetchCmd)
-    }
-    rootCmd.AddCommand(rssCmd)
+		return "", err
+	}
+	rootCmd.SetArgs(args)
+	if err := rootCmd.Execute(); err != nil {
+		return "", err
+	}
 
-    args, err := shlex.Split(strings.TrimPrefix(input, "/"))
-    if err != nil {
-        return "", err
-    }
-    rootCmd.SetArgs(args)
-    if err := rootCmd.Execute(); err != nil {
-        return "", err
-    }
-    
-    return result.String(), errResult
+	return result.String(), errResult
 }
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"fmt"
 	"log"
@@ -28,32 +27,32 @@ func main() {
 		message := cwtchbot.Queue.Next()
 		cid, err := cwtchbot.Peer.FetchConversationInfo(message.Data[event.RemotePeer])
 		if err != nil {
-		  log.Printf("Failed to fetch message: %v\n", err)
+			log.Printf("Failed to fetch message: %v\n", err)
 		} else {
-		switch message.EventType {
-		case event.NewMessageFromPeer:
-			msg := cwtchbot.UnpackMessage(message.Data[event.Data])
-			log.Printf("Received message: %v\n", msg.Data)
-			result, err := dispatch.Run(msg.Data)
-			if err != nil {
-			  log.Printf("Failed to process message: %v\n", err)
-			  result = err.Error()
+			switch message.EventType {
+			case event.NewMessageFromPeer:
+				msg := cwtchbot.UnpackMessage(message.Data[event.Data])
+				log.Printf("Received message: %v\n", msg.Data)
+				result, err := dispatch.Run(msg.Data)
+				if err != nil {
+					log.Printf("Failed to process message: %v\n", err)
+					result = err.Error()
+				}
+				err = sendMessage(cwtchbot, cid, result)
+				if err != nil {
+					log.Printf("Failed to send message: %v\n", err)
+				}
+			case event.ContactCreated:
+				fmt.Printf("Auto approving stranger %v %v\n", cid, message.Data[event.RemotePeer])
+				cwtchbot.Peer.AcceptConversation(cid.ID)
+				sendMessage(cwtchbot, cid, "Hello!")
 			}
-			err = sendMessage(cwtchbot, cid, result)
-			if err != nil {
-			  log.Printf("Failed to send message: %v\n", err)
-		      }
-		case event.ContactCreated:
-			fmt.Printf("Auto approving stranger %v %v\n", cid, message.Data[event.RemotePeer])
-			cwtchbot.Peer.AcceptConversation(cid.ID)
-			sendMessage(cwtchbot, cid, "Hello!")
 		}
-	      }
 	}
 }
 
 func sendMessage(cwtchbot *bot.CwtchBot, cid *model.Conversation, text string) error {
-			msg := string(cwtchbot.PackMessage(model.OverlayChat, text))
-			_, err := cwtchbot.Peer.SendMessage(cid.ID, msg)
-			return err
-		      }
+	msg := string(cwtchbot.PackMessage(model.OverlayChat, text))
+	_, err := cwtchbot.Peer.SendMessage(cid.ID, msg)
+	return err
+}
